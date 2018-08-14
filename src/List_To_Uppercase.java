a765ed34e258db4fad690b89b0ecb8fabf47ce33
Kamil Superson <winpas@wp.pl>
2018-08-14 13:29:31 +0200

    com5

--- src/List_To_Uppercase.java
+++ src/List_To_Uppercase.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class List_To_Uppercase {
     public static void main(String[] args) {
-        List<Strint>list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
     }
 }
